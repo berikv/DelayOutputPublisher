72646d267ba13599858714b0eca4cff0ae73e97e
Berik Visschers <berik@visschers.nu>
2022-01-20 18:04:57 +0100

    Add tests

--- Package.swift
+++ Package.swift
@@ -10,23 +10,22 @@ let package = Package(
         .macOS(.v10_15)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "DelayOutputPublisher",
             targets: ["DelayOutputPublisher"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "VirtualTimeScheduler", path: "../VirtualTimeScheduler")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "DelayOutputPublisher",
             dependencies: []),
         .testTarget(
             name: "DelayOutputPublisherTests",
-            dependencies: ["DelayOutputPublisher"]),
+            dependencies: [
+                "DelayOutputPublisher",
+                "VirtualTimeScheduler"
+            ]),
     ]
 )
--- Tests/DelayOutputPublisherTests/DelayOutputPublisherTests.swift
+++ Tests/DelayOutputPublisherTests/DelayOutputPublisherTests.swift
@@ -1,29 +1,79 @@
 import XCTest
 import Combine
+import VirtualTimeScheduler
 @testable import DelayOutputPublisher
 
 final class DelayOutputPublisherTests: XCTestCase {
-    func testDelay() throws {
-        let expectation = XCTestExpectation()
+    func test_arrayPublisher() throws {
+        let scheduler = VirtualTimeScheduler()
 
-        let df = DateFormatter()
-        df.dateStyle = .none
-        df.timeStyle = .medium
+        var isCompleted = false
+        var receivedAt = [Int]()
 
-        print("Start at \(df.string(from: Date()))")
         let cancellable = (0..<4).publisher
-            .delayOutput(for: .seconds(1), scheduler: RunLoop.main)
+            .delayOutput(for: .seconds(2), scheduler: scheduler)
             .sink(
-                receiveCompletion: {
-                    print("completed \($0)")
-                    expectation.fulfill()
-                },
+                receiveCompletion: { _ in isCompleted = true },
                 receiveValue: { value in
-                    print("Received \(value) after \(df.string(from: Date()))")
+                    receivedAt.append(
+                        Int(scheduler.now.timeIntervalSinceReferenceTime))
                 }
             )
 
-        wait(for: [expectation], timeout: 20)
-        _ = cancellable
+        scheduler.run()
+
+        XCTAssertEqual(receivedAt, [0, 2, 4, 6])
+        XCTAssertTrue(isCompleted)
+
+        _ = cancellable // Fix lifetime
     }
+
+    func test_oneValuePerSecond() throws {
+        let scheduler = VirtualTimeScheduler()
+
+        var receivedValues = [Int]()
+        var receivedTimes = [Int]()
+
+        let subject = PassthroughSubject<Int, Never>()
+        let cancellable = subject
+            .delayOutput(for: .seconds(1), scheduler: scheduler)
+            .sink { value in
+                receivedValues.append(value)
+                receivedTimes.append(
+                    Int(scheduler.now.timeIntervalSinceReferenceTime))
+            }
+
+        subject.send(42)
+        subject.send(101)
+        scheduler.run()
+
+        XCTAssertEqual(receivedValues, [42, 101])
+        XCTAssertEqual(receivedTimes, [0, 1])
+
+        receivedValues.removeAll()
+        receivedTimes.removeAll()
+
+        subject.send(200)
+        subject.send(300)
+        scheduler.advanceTime(by: .seconds(1))
+
+        XCTAssertEqual(receivedValues, [200])
+        XCTAssertEqual(receivedTimes, [2])
+
+        receivedValues.removeAll()
+        receivedTimes.removeAll()
+
+        scheduler.advanceTime(by: .seconds(0))
+
+        XCTAssertEqual(receivedValues, [])
+        XCTAssertEqual(receivedTimes, [])
+
+        scheduler.advanceTime(by: .seconds(1))
+
+        XCTAssertEqual(receivedValues, [300])
+        XCTAssertEqual(receivedTimes, [3])
+
+        _ = cancellable // Fix lifetime
+    }
+
 }
